88366426f0b6fa44b75575cea7c8bacc398663fc
Philip Teclaff <philipteclaff@gmail.com>
2014-10-12 02:22:57 +0100

    shows input now populated from external xml file

--- src/main/java/com/camelcasing/video/ShowListPanel.java
+++ src/main/java/com/camelcasing/video/ShowListPanel.java
@@ -1,15 +1,35 @@
 package com.camelcasing.video;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import javafx.geometry.Insets;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 public class ShowListPanel{
+	
+		private final Logger logger = LogManager.getLogger(getClass());
 
 		private ArrayList<String> shows;
 		private GridPane pane;
+		private File showsFile = new File("/media/camelcasing/ExtraDrive/Java_Files/AirDate/.shows.xml");
 		
 	public ShowListPanel(){
 		createShowList();
@@ -26,11 +46,31 @@ public class ShowListPanel{
 	}
 	
 	public GridPane getShowListPane(){
+
 		return pane;
 	}
 	
 	private void createShowList(){
-		shows = new ArrayList<String>();
+		try {
+			DocumentBuilder docBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+			Document doc = docBuilder.parse(showsFile);
+			XPath xpath = XPathFactory.newInstance().newXPath();
+			String xpathString = "shows/show";
+			XPathExpression xEpr = xpath.compile(xpathString);
+			NodeList results = (NodeList) xEpr.evaluate(doc, XPathConstants.NODESET);
+			shows = new ArrayList<String>(results.getLength());
+				for(int i = 0; i < results.getLength(); i++){
+					shows.add(results.item(i).getTextContent());
+				}
+		
+		} catch (IOException | ParserConfigurationException e) {
+			logger.error("Problem reading shows.xml file");
+		} catch (SAXException e) {
+			logger.error("Problem reading shows.xml file");
+		} catch (XPathExpressionException e) {
+			logger.error("Problem reading shows.xml file");
+		}
+		
 	}
 	
 	public void setShowList(ArrayList<String> shows){

dcaa0c71a00eb56b47d21a2fa135acb042c108cf
Philip Teclaff <philipteclaff@gmail.com>
2015-01-11 10:23:08 +0000

    forgot to re-enable initial check

--- src/main/java/com/camelcasing/video/MasterControl.java
+++ src/main/java/com/camelcasing/video/MasterControl.java
@@ -76,7 +76,7 @@ public class MasterControl implements ChangeListener, FileChooserListener, AddRe
 		root.setCenter(view.getDisplayPane());
 		root.setBottom(progressPane.getProgressPane());
 		
-		//testInternetConnectionAndUpdate();
+		testInternetConnectionAndUpdate();
 	}
 	
 	public boolean standardUpdateIfInternetConnection(){
